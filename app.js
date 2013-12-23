d750b828a740d8f4a3ad49c1cae6bff7fa2dc2fc
Corentin6956 <corentin.kindersurpriz@gmail.com>
2013-12-23 18:41:42 +0100

    Test

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var logfmt = require("logfmt");
 var express = require("express");
 var utilisateurs = require('./routes/utilisateurs');
 var app = express();
 
 
 // Logs
-app.use(logfmt.requestLogger());
+app.use(express.logger());
 
 app.configure(function () {
     //app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
@@ -17,11 +16,11 @@ app.configure(function () {
 
 
 // Routes pour les utilisateurs
-app.get('/utilisateurs', utilisateurs.findAll);
+/*app.get('/utilisateurs', utilisateurs.findAll);
 app.get('/utilisateurs/:id', utilisateurs.findById);
 app.post('/utilisateurs', utilisateurs.addWine);
 app.put('/utilisateurs/:id', utilisateurs.updateWine);
-app.delete('/utilisateurs/:id', utilisateurs.deleteWine);
+app.delete('/utilisateurs/:id', utilisateurs.deleteWine);*/
 
 // Serveur statique
 app.use(express.static(__dirname + '/public'));
