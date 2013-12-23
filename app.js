8135522b92b78e0144534975c6df049e41d1b8dc
Corentin6956 <corentin.kindersurpriz@gmail.com>
2013-12-23 17:34:42 +0100

    Retropédalage

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
 var logfmt = require("logfmt");
 var express = require("express");
+var utilisateurs = require('./routes/utilisateurs');
 var app = express();
 
-app.configure(function () {
-  //app.set('views', __dirname + '/views');
-  //app.set('view engine', 'jade');
-  app.use(app.router);
-  //app.use(express.static(__dirname + '/public'));
-});
-app.configure('development', function () {
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-app.configure('production', function () {
-  app.use(express.errorHandler());
-});
 
 // Logs
 app.use(logfmt.requestLogger());
@@ -21,18 +10,16 @@ app.use(logfmt.requestLogger());
 // Serveur statique
 //app.use(express.static(__dirname + '/public'));
 
+// Routes pour les utilisateurs
+app.get('/utilisateurs', utilisateurs.findAll);
+app.get('/utilisateurs/:id', utilisateurs.findById);
 
 // Montage de l'API REST sur /bookmarks
 app.use('/utilisateurs', app.utilisateurs_app = require('./utilisateurs-rest')());
 
-// Homepage
-/*app.get('/', function (req, res) {
-  res.render('index', { "title": 'Bookmarks' });
-});*/
+
 
 var port = process.env.PORT || 5000;
-
-if (module.parent === null) {
-  app.listen(port);
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-}
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
